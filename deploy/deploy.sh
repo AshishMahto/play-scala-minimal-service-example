34453856d44a613d7a9769bedc8be4d7dd0b7321
Ashish Mahto <ashish_mahto@homedepot.com>
2022-09-02 10:23:13 -0400

    minimal example for GAE standard with java11 fix shell script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,12 @@
-if [[ ! -f app.yaml ]] then
+if [[ ! -f app.yaml ]]; then
   echo "could not find app.yaml, make sure you're in the right directory"
   exit 1
 fi
 
-sbt assembly
+( cd ..; sbt assembly )
 mv ../target/scala-2.13/root-assembly-0.1.0-SNAPSHOT.jar ./example-server.jar
 
-if [[ -z "$1" ]] then
+if [[ -z "$1" ]]; then
   gcloud app deploy
 else
   gcloud app deploy --project="$1"
